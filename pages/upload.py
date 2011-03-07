96020842e4b9a62107db1cb0fdecbe45eaa1fc2f
Erik van Zijst <evzijst@atlassian.com>
2011-03-08 00:18:07 +1100

    Adding restkit and api call to create a new remote repository.

--- pages/upload.py
+++ pages/upload.py
@@ -1,6 +1,12 @@
 __author__ = 'erik'
 
 from PyQt4 import QtGui, QtCore
+from restkit import request, BasicAuth
+from restkit.forms import form_encode
+
+class RemoteException(Exception):
+	'''Raised when a remote call failed.'''
+	pass
 
 class WizardPage(QtGui.QWizardPage):
 
@@ -11,7 +17,7 @@ class WizardPage(QtGui.QWizardPage):
         grid = QtGui.QGridLayout()
 
         description = QtGui.QLabel('<qt><p>Enter your Bitbucket username and password below, '
-                'as well as name for the repository and we\'ll upload it '
+                'as well as a name for the new repository and we\'ll upload it '
                 'straight to Bitbucket!</p>\n'
                 '<p>If you don\'t have a Bitbucket account yet, <a href="https://bitbucket.org/account/signup/">click here to '
                 'sign up for free</a>.<p><qt>')
@@ -36,3 +42,20 @@ class WizardPage(QtGui.QWizardPage):
         grid.addWidget(repoName, 4, 1)
 
         self.setLayout(grid)
+
+	def _create_repository(self, username, password, repo_name):
+		'''Makes a REST call to Bitbucket to create the new remote repository
+		before we can push.'''
+		resp = None
+		try:
+			resp = request('https://api.bitbucket.org/1.0/repositories/',
+				method='POST',
+				body=form_encode({'name': repo_name}),
+				filters=[BasicAuth(username, password)],
+				headers={'Content-Type': 'application/x-www-form-urlencoded'},
+				follow_redirects=True)
+		except:
+			raise RemoteException()
+
+		if (resp.status_int // 100) is not 2:
+			raise RemoteException('Unexpected response: ' + resp.body_string)
