653090df2a357ca7415ab6b2ca8430ea7743b1d2
Erik van Zijst <evzijst@atlassian.com>
2011-03-07 19:17:05 +1100

    Added modal login progress dialog.

--- pages/upload.py
+++ pages/upload.py
@@ -1,5 +1,6 @@
 __author__ = 'erik'
 
+import time
 from PyQt4 import QtGui, QtCore
 
 class WizardPage(QtGui.QWizardPage):
@@ -30,6 +31,7 @@ class WizardPage(QtGui.QWizardPage):
         passwordEdit = QtGui.QLineEdit()
         passwordEdit.setEchoMode(QtGui.QLineEdit.Password)
         grid.addWidget(passwordEdit, 3, 1)
+        QtGui.QWizardPage.registerField(self, 'bb_password', passwordEdit)
 
         repoName = QtGui.QLabel('Repository Name')
         grid.addWidget(repoName, 4, 0)
@@ -37,4 +39,58 @@ class WizardPage(QtGui.QWizardPage):
         grid.addWidget(repoName, 4, 1)
         QtGui.QWizardPage.registerField(self, 'bb_reponame*', repoName)
 
+
         self.setLayout(grid)
+
+    def validatePage(self):
+
+        # start the background job that logs us in
+        self.job = WizardPage.LoginJob(self)
+        self.connect(self.job, QtCore.SIGNAL('login_successful(PyQt_PyObject)'), self.login_successful)
+        self.connect(self.job, QtCore.SIGNAL('login_failed(PyQt_PyObject)'), self.login_failed)
+        self.job.start()
+        return False
+
+        print "validated!"
+
+    def login_successful(self):
+        '''Username and password worked, we created the new remote repo, so
+        take us to the next screen and commence the push!'''
+
+        print "Login successful."
+
+    def login_failed(self, message):
+        '''Print error message'''
+        print 'Login failed: ' + message
+
+    class LoginJob(QtCore.QThread):
+
+        # Signals:
+        _progress = QtCore.pyqtSignal(int, name='progress')
+
+        def __init__(self, parent):
+            QtCore.QThread.__init__(self)
+
+            self.parent = parent
+
+            self.dialog = QtGui.QProgressDialog('Logging in...', 'Cancel', 0, 2, parent=parent)
+            self.dialog.setWindowModality(QtCore.Qt.WindowModal)
+            self.dialog.setAutoClose(False)
+            self.dialog.setAutoReset(False)
+
+            self.connect(self, QtCore.SIGNAL('progress(int)'), self.dialog, QtCore.SLOT('setValue(int)'))
+            self.connect(self, QtCore.SIGNAL('setLabel(const QString &)'), self.dialog, QtCore.SLOT('setLabelText(const QString &)'))
+            self.dialog.show()
+
+        def run(self):
+            try:
+                self._progress.emit(0)
+                time.sleep(1)
+                self.emit(QtCore.SIGNAL('setLabel(const QString &)'), 'Creating repository...')
+
+                self._progress.emit(1)
+                time.sleep(1)
+                self._progress.emit(2)
+                self.emit(QtCore.SIGNAL('login_successful(PyQt_PyObject)'), None)
+            except:
+                self.emit(QtCore.SIGNAL('login_failed(PyQt_PyObject)'), None)
