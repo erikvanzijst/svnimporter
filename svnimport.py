d7c1aac36fc36908f012c3d87d66237ae384f9c4
Erik van Zijst <evzijst@atlassian.com>
2011-03-08 22:08:56 +1100

    Trying to get a spinner dialog working here..

--- svnimport.py
+++ svnimport.py
@@ -2,16 +2,17 @@ __author__ = 'erik'
 
 import sys
 from PyQt4 import QtGui
-from pages import svnurl, mapping, output, importer, upload
+from pages import svnurl, mapping, output, importer, createrepo, upload
 
 app = QtGui.QApplication(sys.argv)
 
 wizard = QtGui.QWizard()
 wizard.setWindowTitle('Subversion to Mercurial Converter')
-wizard.addPage(svnurl.WizardPage())
-wizard.addPage(mapping.WizardPage())
-wizard.addPage(output.WizardPage())
-wizard.addPage(importer.WizardPage())
+#wizard.addPage(svnurl.WizardPage())
+#wizard.addPage(mapping.WizardPage())
+#wizard.addPage(output.WizardPage())
+#wizard.addPage(importer.WizardPage())
+wizard.addPage(createrepo.WizardPage())
 wizard.addPage(upload.WizardPage())
 wizard.show()
 
